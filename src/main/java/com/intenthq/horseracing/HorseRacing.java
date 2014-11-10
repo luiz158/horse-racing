e14f13911693ab6719ffc484e283ad334cbc501f
antmyth <antonio.p.nascimento@gmail.com>
2014-11-10 09:30:26 +0000

    Initial commit, with ignored acceptance test

---
+++ src/main/java/com/intenthq/horseracing/HorseRacing.java
@@ -0,0 +1,11 @@
+package com.intenthq.horseracing;
+
+/**
+ * Created by antmyth on 10/11/14.
+ */
+public class HorseRacing {
+
+    public String startRaceWith(String sampleInput) {
+        return null;
+    }
+}
---
+++ src/test/java/com/intenthq/horseracing/ExampleAcceptanceTest.java
@@ -0,0 +1,35 @@
+package com.intenthq.horseracing;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@Ignore
+public class ExampleAcceptanceTest {
+    private static final String SAMPLE_INPUT = "Star, Dakota, Cheyenne, Misty, Spirit\n"+
+            "1 60\n"+
+            "3 5\n"+
+            "1 60\n"+
+            "4 5\n"+
+            "4 10\n"+
+            "2 5\n"+
+            "5 10\n"+
+            "1 60\n"+
+            "3 20\n"+
+            "7 10\n"+
+            "1 40\n"+
+            "2 60";
+    private static final String SAMPLE_OUTPUT = "Position, Lane, Horse name\n"+
+            "1, 1, Star\n"+
+            "2, 3, Cheyenne\n"+
+            "3, 4, Misty\n"+
+            "4, 5, Spirit\n"+
+            "5, 2, Dakota";
+
+    @Test
+    public void shouldReturnTheExpectedOutputForTheSampleInput(){
+        assertThat(new HorseRacing().startRaceWith(SAMPLE_INPUT),is(SAMPLE_OUTPUT));
+    }
+}

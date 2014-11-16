a8754cdad2a568b9c804f9a92872cf1f28e66f03
Darren Seeto <darrens@yazino.com>
2014-11-16 18:09:12 +0000

    tidied up outputwriter class

--- src/main/java/com/intenthq/horseracing/OutputWriter.java
+++ src/main/java/com/intenthq/horseracing/OutputWriter.java
@@ -5,26 +5,28 @@ import java.util.List;
 public class OutputWriter {
 
     private static final String HEADING = "Position, Lane, Horse name\n";
+    public static final int FIRST = 1;
+    public static final String COMMA_DELIMITER = ", ";
+    public static final String NEW_LINE_DELIMITER = "\n";
 
     public String print(final List<Horse> sortedHorseList) {
-        final StringBuilder sb = new StringBuilder(HEADING);
+        final StringBuilder stringBuilder = new StringBuilder(HEADING);
 
         int position = 0;
         int previousHorseDistanceCovered = 0;
         for (Horse horse : sortedHorseList) {
             position++;
             if (horseTiedWithPreviousHorse(position, previousHorseDistanceCovered, horse)) {
-                printLaneEntry(sb, position - 1, horse);
+                printLaneEntry(stringBuilder, position - 1, horse);
             } else {
-                printLaneEntry(sb, position, horse);
+                printLaneEntry(stringBuilder, position, horse);
             }
 
             previousHorseDistanceCovered = horse.getYardsCovered();
 
         }
 
-        removeTrailingNewLine(sb);
-        return sb.toString();
+        return stringBuilder.toString().trim();
 
     }
 
@@ -32,16 +34,12 @@ public class OutputWriter {
                                                final int previousHorseDistanceCovered,
                                                final Horse horse) {
 
-        return previousHorseDistanceCovered == horse.getYardsCovered() && position != 1;
+        return previousHorseDistanceCovered == horse.getYardsCovered() && position != FIRST;
     }
 
     private void printLaneEntry(final StringBuilder sb, final int position, final Horse horse) {
-        sb.append(String.valueOf(position)).append(", ");
-        sb.append(horse.getLane()).append(", ");
-        sb.append(horse.getHorseName()).append("\n");
-    }
-
-    private void removeTrailingNewLine(final StringBuilder sb) {
-        sb.setLength(sb.length() - 1);
+        sb.append(String.valueOf(position)).append(COMMA_DELIMITER);
+        sb.append(horse.getLane()).append(COMMA_DELIMITER);
+        sb.append(horse.getHorseName()).append(NEW_LINE_DELIMITER);
     }
 }

eab71b96324faa3ce72bce312020732e0db6b319
Darren Seeto <darrens@yazino.com>
2014-11-17 23:33:36 +0000

    added logback for logging purposes

--- src/main/java/com/intenthq/horseracing/InputProcessor.java
+++ src/main/java/com/intenthq/horseracing/InputProcessor.java
@@ -3,8 +3,9 @@ package com.intenthq.horseracing;
 import com.google.common.base.Splitter;
 import com.intenthq.horseracing.exception.BallTossInvalidException;
 import com.intenthq.horseracing.exception.NoValidHorsesException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -14,9 +15,11 @@ import static com.google.common.collect.Maps.newHashMap;
 
 @Component
 public class InputProcessor {
+    final static Logger LOG = LoggerFactory.getLogger(InputProcessor.class);
 
     public static final int BALL_TOSS_DISTANCE_INDEX = 1;
     public static final int BALL_TOSS_LANE_INDEX = 0;
+    public static final String HORSE_DELIMITER = ",";
     private final InputValidator validator;
 
     @Autowired
@@ -45,19 +48,19 @@ public class InputProcessor {
         while (scan.hasNextLine() && !raceFinished) {
             try {
                 String ballTossString = scan.nextLine();
+                LOG.info("processing BallToss: {}", ballTossString);
                 validator.validateBallToss(ballTossString, horseMap.size());
                 String[] ballToss = ballTossString.split("\\s");
 
                 Horse horse = horseMap.get(Integer.valueOf(ballToss[BALL_TOSS_LANE_INDEX]));
                 horse.addYards(Integer.parseInt(ballToss[BALL_TOSS_DISTANCE_INDEX]));
 
-                if(horse.isFinished()) {
+                if (horse.isFinished()) {
                     raceFinished = true;
                 }
 
             } catch (BallTossInvalidException e) {
-                // log an error message
-                // but continue processing
+                LOG.warn("skipping ball toss: {}", e.getMessage());
             }
         }
 
@@ -66,10 +69,11 @@ public class InputProcessor {
     }
 
     private Iterable<String> splitHorsesOnCommas(String horseLanesString) {
-        return Splitter.on(",").trimResults().split(horseLanesString);
+        return Splitter.on(HORSE_DELIMITER).trimResults().split(horseLanesString);
     }
 
     private void addHorseToMap(Map<Integer, Horse> horseMap, String horseName, int lane) {
+        LOG.info("adding horse: {} lane: {}", horseName, lane);
         Integer horseKey = new Integer(lane);
         Horse horse = new Horse(lane, horseName);
         horseMap.put(horseKey, horse);
--- src/main/java/com/intenthq/horseracing/exception/HorseRacingInputException.java
+++ src/main/java/com/intenthq/horseracing/exception/HorseRacingInputException.java
@@ -1,8 +1,5 @@
 package com.intenthq.horseracing.exception;
 
-/**
- * Created by darrens on 16/11/2014.
- */
 public interface HorseRacingInputException {
     String getCode();
 
